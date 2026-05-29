605556b0582e79cd87828a4cc5f0979b26399032
Bhuvneshwari Patil <bhuvneshwaripatil250@gmail.com>
2026-05-29 15:37:09 +0530

    STD_CRUD_2

---
+++ src/app/shared/consts/students.ts
@@ -0,0 +1,42 @@
+export let studentsData1 = [
+    {
+        stdID: "S101",
+        fname: "Aarav",
+        lname: "Sharma",
+        email: "aarav@gmail.com",
+        contact: "9876543210",
+        isActive: false
+    },
+    {
+        stdID: "S102",
+        fname: "Anaya",
+        lname: "Patil",
+        email: "anaya@gmail.com",
+        contact: "9876543211",
+        isActive: true
+    },
+    {
+        stdID: "S103",
+        fname: "Vivaan",
+        lname: "Joshi",
+        email: "vivaan@gmail.com",
+        contact: "9876543212",
+        isActive: true
+    },
+    {
+        stdID: "S104",
+        fname: "Ishita",
+        lname: "Kulkarni",
+        email: "ishita@gmail.com",
+        contact: "9876543213",
+        isActive: false
+    },
+    {
+        stdID: "S105",
+        fname: "Aditya",
+        lname: "Deshmukh",
+        email: "aditya@gmail.com",
+        contact: "9876543214",
+        isActive: true
+    }
+];
